4f7ca793fd25f95aaf4a6f1172bdc15702fc490d
tiranno <pedelose.1@osu.edu>
2016-02-10 15:41:56 -0500

    Added scss file and more

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,6 @@
 $(function () {
+  /* Fucntions for updating the dialog stream */
+    // Websocket with server to preprocess sumbissions before sending to Watson
     var ws = new WebSocket('ws://' + window.location.host + '/ws');
     ws.onopen = function() {
     };
@@ -18,13 +20,13 @@ $(function () {
                 text: evt.data
             }))
         }
-        $('#dialog-pane').prepend(answer);
+        $('#center-dialog').prepend(answer);
     }
-
+    // Upon submission of a quesiton
     $('form').submit(function(){
-        var question_text = $('#mainquerybar').val();
+        var question_text = $('#query-bar').val();
         ws.send(question_text);
-        $('#mainquerybar').val('');
+        $('#query-bar').val('');
         var question = $('<div />', {
             'class': 'col-xs-12'
         })
@@ -39,14 +41,16 @@ $(function () {
             'class': 'col-xs-12'
         })
         line_break.append('<hr>');
-        $('#dialog-pane').prepend(line_break);
-        $('#dialog-pane').prepend(question);
+        $('#center-dialog').prepend(line_break);
+        $('#center-dialog').prepend(question);
         // Prevents page from reloading
         return false;
     });
-
+    // Submit button for input is clicked
     $('#ask-button').click(function() {
-        ws.send($('#mainquerybar').val());
-        $('#mainquerybar').val('');
+        ws.send($('#query-bar').val());
+        $('#query-bar').val('');
     });
+
+    
 });
