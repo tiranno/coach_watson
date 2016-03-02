60bf1ef0849e3bbf0b0f6f350ae67d8a60f1926b
tiranno <pedelose.1@osu.edu>
2016-03-02 01:59:04 -0500

    Module for ui js

--- static/js/ui_module.js
+++ static/js/ui_module.js
@@ -1,21 +1,23 @@
 $(function () {
+  /* JQuery mobile overrides */
+  $( document ).ready(function() {
+    $.mobile.loadingMessage = false;
+  })
+
   /* Side drawer functions*/
-    var drawerExpand = function() {
-      $('#panel-left').addClass('expanded');
-      $('#page-mask').addClass('visible');
-      return;
-    };
-    var drawerClose = function() {
-      $('#panel-left').removeClass('expanded');
-      $('#page-mask').removeClass('visible');
-      return;
-    };
+  var drawerExpand = function() {
+    $('#panel-left').addClass('expanded');
+    $('#page-mask').addClass('visible');
+    return;
+  };
+  var drawerClose = function() {
+    $('#panel-left').removeClass('expanded');
+    $('#page-mask').removeClass('visible');
+    return;
+  };
 
-    $('#panel-center').on("swipe", drawerExpand);
-
-    $('#page-mask').on('tap click', drawerClose);
-
-    $('#drawer-exit').on('tap click', drawerClose);
-
-    $('#input-menu').on('tap click', drawerExpand);
+  $('#panel-center').on("swipe", drawerExpand);
+  $('#page-mask').on('tap click', drawerClose);
+  $('#drawer-exit').on('tap click', drawerClose);
+  $('#input-menu').on('tap click', drawerExpand);
 });
