4e78042941da506ad5f714622383233bfc29cb30
tiranno <pedelose.1@osu.edu>
2016-03-02 00:31:06 -0500

    Fixes for Jquery mobile

--- static/js/index.js
+++ static/js/index.js
@@ -53,24 +53,22 @@ $(function () {
     });
 
   /* Side drawer functions*/
-    $('#page-mask').on('tap click', function() {
-      if($('#panel-left').hasClass('expanded')) {
-          $('#panel-left').removeClass('expanded');
-      }
+    var drawerExpand = function() {
+      $('#panel-left').addClass('expanded');
+      $('#page-mask').addClass('visible');
+      return;
+    };
+    var drawerClose = function() {
+      $('#panel-left').removeClass('expanded');
       $('#page-mask').removeClass('visible');
-    });
-    $('#drawer-exit').on('tap click', function() {
-      if($('#panel-left').hasClass('expanded')) {
-          $('#panel-left').removeClass('expanded');
-      }
-      $('#page-mask').removeClass('visible');
-    });
-    $('#input-menu').on('tap click', function() {
-        if($('#panel-left').hasClass('expanded')) {
-            $('#panel-left').removeClass('expanded');
-        } else {
-            $('#panel-left').addClass('expanded');
-        }
-        $('#page-mask').addClass('visible');
-    });
+      return;
+    };
+
+    $('#panel-center').on("swipe", drawerExpand);
+
+    $('#page-mask').on('tap click', drawerClose);
+
+    $('#drawer-exit').on('tap click', drawerClose);
+
+    $('#input-menu').on('tap click', drawerExpand);
 });
