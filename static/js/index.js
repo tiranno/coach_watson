db5f3872abead2a38e0c41c83b2c4df8328c5d79
tiranno <pedelose.1@osu.edu>
2016-03-02 19:56:38 -0500

    Updates to swipe gesture handling

--- static/js/index.js
+++ static/js/index.js
@@ -51,24 +51,4 @@ $(function () {
         // Prevents page from reloading
         return false;
     });
-
-  /* Side drawer functions*/
-    var drawerExpand = function() {
-      $('#panel-left').addClass('expanded');
-      $('#page-mask').addClass('visible');
-      return;
-    };
-    var drawerClose = function() {
-      $('#panel-left').removeClass('expanded');
-      $('#page-mask').removeClass('visible');
-      return;
-    };
-
-    $('#panel-center').on("swipe", drawerExpand);
-
-    $('#page-mask').on('tap click', drawerClose);
-
-    $('#drawer-exit').on('tap click', drawerClose);
-
-    $('#input-menu').on('tap click', drawerExpand);
 });
