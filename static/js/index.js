c0dce1f0584513bc77c9ae148db3ca80672fa69e
tiranno <pedelose.1@osu.edu>
2016-03-07 07:52:22 -0500

    Added updated card styles to our javascript

--- static/js/index.js
+++ static/js/index.js
@@ -1,13 +1,16 @@
 $(function () {
   /* Fucntions for updating the dialog stream */
     // Websocket with server to preprocess sumbissions before sending to Watson
-    var ws = new WebSocket('wss://' + window.location.host + '/ws');
+    var ws = new WebSocket('ws://' + window.location.host + '/ws');
     ws.onopen = function() {
     };
     ws.onmessage = function(evt) {
         var answer = $('<div />', {
-            'class': 'card card-answer bottom-16'
+            'class': 'col-xs-12'
         });
+        answer.append($('<div />', {
+            'class': 'card card-answer bottom-16'
+        }));
         if(evt.data[0] === '<') {
             answer.children(0).append($('<div class="card-heading">I think this might work for you ...</div>'));
             answer.children(0).append($(evt.data));
@@ -25,20 +28,23 @@ $(function () {
         ws.send(question_text);
         $('#query-bar').val('');
         var question = $('<div />', {
+            'class': 'col-xs-12'
+        });
+        question.append($('<div />', {
             'class': 'card card-question bottom-16'
-        })
+        }));
         question.children(0).append($('<div />', {
             'class': 'card-body',
             text: question_text
         }))
-        var line_break = $('<hr>');
+        var line_break = $('<hr/>')
         $('#center-dialog').prepend(line_break);
         $('#center-dialog').prepend(question);
         // Unhide the center-dialog and hide the placeholder
         if(!$('#center-dialog').hasClass('visible')) {
             $('#center-dialog').addClass('visible');
-            $('#no-content').addClass('invisible');
         }
+        $('#no-content').addClass('invisible');
         // Prevents page from reloading
         return false;
     });
