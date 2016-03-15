e387a008c3a738c1dab0cb75c4c72269164b2452
tiranno <pedelose.1@osu.edu>
2016-03-15 13:21:17 -0400

    Feedback functional

--- static/js/ui_module.js
+++ static/js/ui_module.js
@@ -1,55 +1,62 @@
 $(function () {
-  /* Side drawer functions*/
-  var drawerExpand = function() {
-    $('#panel-left').addClass('expanded');
-    window.setTimeout(deB,100);
-    return;
-  };
-  var deB = function() {
-    $('#page-mask').addClass('visible');
-  }
-  var drawerClose = function() {
-    $('#panel-left').removeClass('expanded');
-    $('#page-mask').removeClass('visible');
-    return;
-  };
-  var inputClear = function() {
-    $('#query-bar').val('');
-  }
+    /* Side drawer functions*/
+    var drawerExpand = function() {
+        $('#panel-left').addClass('expanded');
+        window.setTimeout(deB,100);
+        return;
+    };
+    var deB = function() {
+        $('#page-mask').addClass('visible');
+    };
+    var drawerClose = function() {
+        $('#panel-left').removeClass('expanded');
+        $('#page-mask').removeClass('visible');
+        return;
+    };
+    var inputClear = function() {
+        $('#query-bar').val('');
+    };
 
-  /* Event listeners */
-  // Issues with sideway scrolling currently (get rid of sideways scroll)
-  // $('#panel-center').on('swiperight', drawerExpand);
-  // $('#panel-center').on('swipeleft', drawerClose);
-  $('#page-mask').on('tap click', drawerClose);
-  $('#input-menu').on('tap click', drawerExpand);
+    /* Event listeners */
+    // Issues with sideway scrolling currently (get rid of sideways scroll)
+    // $('#panel-center').on('swiperight', drawerExpand);
+    // $('#panel-center').on('swipeleft', drawerClose);
+    $('#page-mask').on('tap click', drawerClose);
+    $('#input-menu').on('tap click', drawerExpand);
 
-  $('#input-clear').on('tap click', inputClear);
+    $('#input-clear').on('tap click', inputClear);
 
 
-  /* Dialog functions */
-  var showFeedbackDialog = function() {
-    $('#feedback-dialog').addClass('visible');
-  }
-  var hideFeedbackDialog = function() {
-    $('#feedback-dialog').removeClass('visible');
-    $('#feedback-text').empty();
-    $('#fb-text').empty();
-  }
-  $('#feedback').on('tap click', showFeedbackDialog);
-  $('#fb-clear').on('tap click', hideFeedbackDialog);
-  $('#feedback-form').submit(function() {
-    input = ("#fb-text").html();
-    console.log(input);
-    if(x == null || x == ""){
-      return false;
-    }
-    $('#feedback-text').val(("#fb-text").html());
-    hideFeedbackDialog();
-    return false;
-  });
-  // $('#.fb-text').change(function(){
-  //   ('#feedback-text').val(("#fb-text").html());
-  // });
+    /* Dialog functions */
+    var showFeedbackDialog = function() {
+        $('#feedback-dialog').addClass('visible');
+    };
+    var hideFeedbackDialog = function() {
+        $('#feedback-dialog').removeClass('visible');
+        $('#feedback-text').empty();
+        $('#fb-text').empty();
+    };
+    $('#feedback').on('tap click', showFeedbackDialog);
+    $('#fb-clear').on('tap click', hideFeedbackDialog);
+    $('#feedback-form').submit(function(event) {
+        input = $("#fb-text").html();
+        if(input == null || input == ""){
+          return false;
+        }
+        $('#feedback-text').val($("#fb-text").html());
+
+        $.ajax({
+          url:'/form/feedback',
+          type:'post',
+          data:$('#feedback-form').serialize(),
+          success:function(){
+              hideFeedbackDialog();
+          }
+        });
+        return false;
+    });
+    // $('#.fb-text').change(function(){
+    //   ('#feedback-text').val(("#fb-text").html());
+    // });
 
 });
