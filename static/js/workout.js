d72a86d8eba03f8e5e3d5d79619b9183c91ea7c5
Brian Kelly <brianpkelly46@gmail.com>
2016-04-19 00:11:21 -0400

    Workout frontend: add button functionality

--- static/js/workout.js
+++ static/js/workout.js
@@ -34,18 +34,45 @@ $(function () {
         }) );
         var subrow2 = $('<div />', {'class': 'col-xs-3'});
         subrow2.append( $('<button />', {
-            'id': 'cancel-workout',
+            'id': 'cancel-add-workout',
             'class': 'md-button flat',
             'style': 'float: right;',
             'html': '<i class="material-icons">cancel</i>'
         }) );
         subrow2.append( $('<button />', {
-            'id': 'confirm-workout',
+            'id': 'confirm-add-workout',
             'class': 'md-button flat',
             'style': 'float: right;',
             'html': '<i class="material-icons">check</i>'
         }) );
-        var editing = $('<li />', {'id': '?', 'class': 'visible'}).append(subrow0, subrow1, subrow2);
+        var editing = $('<li />', {'id': '?', 'class': 'visible'}).append('<a />');
+        editing.children().eq(0).append(subrow0, subrow1, subrow2);
+
+        subrow2.children().eq(0).on('click tap', function () {
+            editing.remove();
+        });
+
+        subrow2.children().eq(1).on('click tap', function () {
+            editing.children().eq(0).children().remove();
+
+            var workout_name = subrow0.children().val();
+            var amount = subrow1.children().val();
+
+            subrow0.children().remove();
+            subrow1.children().remove();
+            subrow2.children().remove();
+
+            subrow0.append(workout_name);
+            subrow1.append(amount);
+            subrow2.append( $('<button />', {
+                'id': 'confirm-add-workout',
+                'class': 'md-button flat',
+                'style': 'float: right;',
+                'html': '<i class="material-icons">mode_edit</i>'
+            }) );
+
+            editing.children().eq(0).append(subrow0, subrow1, subrow2);
+        });
 
         $('#workouts-list > ul').append(editing);
     }
